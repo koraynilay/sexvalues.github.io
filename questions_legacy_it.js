182406919e575ed9a7cf3a74e88b4db0af930b05
koraynilay <koray.fra@gmail.com>
2023-05-18 12:25:30 +0200

    Add italian translation in questions_legacy_it.js

---
+++ questions_legacy_it.js
@@ -0,0 +1,228 @@
+questions = [
+    // Attract
+    {
+        "id":1,
+        "question": "Sarei disposto ad avere relazioni con uomini.",
+        "question_en": "I would have relationships with men.",
+        "effect": {
+            "attract": 1,
+        }
+    },
+    {
+        "id":2,
+        "question": "Potrei immagiarmi in uno scenario sessuale con un uomo.",
+        "question_en": "I could imagine myself in a sexual scenario with a man.",
+        "effect": {
+            "attract": 1,
+        }
+    },
+    {
+        "id":3,
+        "question": "Sono attratto da corpi muscolosi.",
+        "question_en": "I find myself attracted to muscular bodies.",
+        "effect": {
+            "attract": 1,
+        }
+    },
+    {
+        "id":4,
+        "question": "Il pipo sarebbe solo meglio! (mi piace il cazzo)",
+        "question_en": "The benis only makes it better! (I like dick)",
+        "effect": {
+            "attract": 1,
+        }
+    },
+    {
+        "id":5,
+        "question": "Sono attratto dalla pelle morbida e da vestiti femminili.",
+        "question_en": "I find myself attracted to soft skin, feminine clothing.",
+        "effect": {
+            "attract": -1,
+        }
+    },
+    {
+        "id":6,
+        "question": "Potrei immagiarmi in una scenario sessuale con una donna.",
+        "question_en": "I could imagine myself in a sexual scenario with a woman.",
+        "effect": {
+            "attract": -1,
+        }
+    },
+    {
+        "id":7,
+        "question": "Potrei avere relazioni con donne.",
+        "question_en": "I would have relationships with women.",
+        "effect": {
+            "attract": -1,
+        }
+    },
+
+    // Drive
+    {
+        "id":8,
+        "question": "A volte mi ritrovo ad immaginare scenari sessuali.",
+        "question_en": "I sometimes find myself dreaming about lewd scenarios.",
+        "effect": {
+            "drive": 1,
+        }
+    },
+    {
+        "id":9,
+        "question": "Avrei sesso regolarmente.",
+        "question_en": "I would have sex on a regular basis.",
+        "effect": {
+            "drive": 1,
+        }
+    },
+    {
+        "id":10,
+        "question": "Mi masturbo almeno una volta al giorno.",
+        "question_en": "I masturbate at least once a day.",
+        "effect": {
+            "drive": 1,
+        }
+    },
+    {
+        "id":11,
+        "question": "Guardo contenuti pornografici.",
+        "question_en": "I watch pornographic content.",
+        "effect": {
+            "drive": 1,
+        }
+    },
+    {
+        "id":12,
+        "question": "Mi trovo disgustato dall'idea del sesso.",
+        "question_en": "I find myself repulsed by the idea of sex.",
+        "effect": {
+            "drive": -2,
+        }
+    },
+    {
+        "id":13,
+        "question": "Posso dire che la castità sia la mia virtù.",
+        "question_en": "I could say 'chastity' is my virtue.",
+        "effect": {
+            "drive": -2,
+        }
+    },
+
+    // Dominant
+    {
+        "id":14,
+        "question": "Mi piace l'idea di avere potere su un'altra persona.",
+        "question_en": "I like the idea of having power over someone.",
+        "effect": {
+            "dominant": 1,
+        }
+    },
+    {
+        "id":15,
+        "question": "Mi piace (o potrebbe piacere) essere sotto il controllo di qualcuno.",
+        "question_en": "I (would) enjoy someone having control over me.",
+        "effect": {
+            "dominant": -1,
+        }
+    },
+    {
+        "id":16,
+        "question": "Non sarei a mio agio fossi dominato.",
+        "question_en": "I wouldn't be comfortable with getting dominated.",
+        "effect": {
+            "dominant": 1,
+        }
+    },
+
+    // Deviance
+    {
+        "id":17,
+        "question": "Ho dei kink/fetish non visti bene dalla società.",
+        "question_en": "I have frowned-upon kinks/fetishes.",
+        "effect": {
+            "deviance": 1,
+        }
+    },
+    {
+        "id":18,
+        "question": "Faccio parte di sotto-comunità associate a devianze.",
+        "question_en": "I belong to sub-communities associated with deviances.",
+        "effect": {
+            "deviance": 1,
+        }
+    },
+    {
+        "id":19,
+        "question": "Mi piacerebbe essere insultato oppure insultare.",
+        "question_en": "I (would) enjoy degrading or getting degraded.",
+        "effect": {
+            "deviance": 1,
+        }
+    },
+    {
+        "id":20,
+        "question": "Mi piace (o potrebbe piacere) infliggere dolore o riceverlo.",
+        "question_en": "I (would) enjoy inflicting pain or having pain inflicted on me.",
+        "effect": {
+            "deviance": 1,
+        }
+    },
+    {
+        "id":21,
+        "question": "Mi considero normale (come gusti sessuali).",
+        "question_en": "I consider myself vanilla.",
+        "effect": {
+            "deviance": -2,
+        }
+    },
+    {
+        "id":22,
+        "question": "Trovo la maggior parte dei fetish disgustosi.",
+        "question_en": "I find most of fetishes disgusting.",
+        "effect": {
+            "deviance": -1,
+        }
+    },
+    {
+        "id":23,
+        "question": "Potrei essere troppo noioso per qualcuno a letto.",
+        "question_en": "I might be too boring for some people in the bedroom.",
+        "effect": {
+            "deviance": -1,
+        }
+    },
+
+    // Affect
+    {
+        "id":24,
+        "question": "Potrei essere lascivo solo con il mio partner (o i miei partner).",
+        "question_en": "I could only be lewd to my partner(s).",
+        "effect": {
+            "affect": 1,
+        }
+    },
+    {
+        "id":25,
+        "question": "Il sesso dovrebbe essere un argomento romantico.",
+        "question_en": "Sex should be a romantic topic.",
+        "effect": {
+            "affect": 1
+        }
+    },
+    {
+        "id":26,
+        "question": "Mi vanno bene rapporti occasionali e cose da una notte.",
+        "question_en": "I am okay with the idea of hook-ups or one-night stands.",
+        "effect": {
+            "affect": -1
+        }
+    },
+    {
+        "id":27,
+        "question": "Nel sesso, il piacere è più importante che l'intimità.",
+        "question_en": "Sex is more about the pleasure than the intimacy.",
+        "effect": {
+            "affect": -1
+        }
+    }
+
+];
